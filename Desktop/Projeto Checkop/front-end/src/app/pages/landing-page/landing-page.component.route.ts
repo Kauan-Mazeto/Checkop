666d42e44f0e8e5104ef16a86dcefb10a47ce477
Kauan <kauan.mazetorodrigues@gmail.com>
2026-08-18 00:42:38 -0300

    teste

--- Desktop/Projeto Checkop/front-end/src/app/pages/landing-page/landing-page.component.route.ts
+++
@@ -1,7 +0,0 @@
-import { Route } from '@angular/router';
-import { LandingPageComponent } from './landing-page.component';
-
-export const landingPageRoute: Route = {
-  path: 'landing',
-  component: LandingPageComponent,
-};
