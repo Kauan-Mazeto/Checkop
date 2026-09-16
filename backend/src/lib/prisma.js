b059ac8d33fb6415bfb322468e8c73ff59174a3e
Luidi Augusto <luidi.barancelli@escola.pr.gov.br>
2026-09-16 09:26:59 -0300

    Ajuste de type

--- backend/src/lib/prisma.js
+++ backend/src/lib/prisma.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
-import { PrismaClient } from '@prisma/client';
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
 
 const adapter = new PrismaBetterSqlite3({
   url: process.env.DATABASE_URL,
